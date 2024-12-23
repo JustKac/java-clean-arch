d10c0b68287f1f2b57a51d50c7ef5ab8a3dd4a39
Caio Henrique Cordeiro Lins <caio.henrique@segsat.com>
2024-12-23 16:07:24 -0300

    adiciona anotação de coluna única para CPF na entidade de usuário

--- codechella/src/main/java/br/com/justkac/codechella/infra/persistence/UsuarioEntity.java
+++ codechella/src/main/java/br/com/justkac/codechella/infra/persistence/UsuarioEntity.java
@@ -1,12 +1,13 @@
 package br.com.justkac.codechella.infra.persistence;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "usuarios")
@@ -15,6 +16,8 @@ public class UsuarioEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(unique = true)
     private String cpf;
     private String nome;
     private LocalDate nascimento;
