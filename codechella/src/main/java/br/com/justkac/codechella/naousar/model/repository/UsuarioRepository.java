49bec3720595fc1f4dd7e6e239b2f2f362f49094
Caio Henrique Cordeiro Lins <caio.henrique@segsat.com>
2024-12-23 15:35:47 -0300

    remove classes e interfaces relacionadas ao controle e serviço de usuários

--- codechella/src/main/java/br/com/justkac/codechella/naousar/model/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.justkac.codechella.naousar.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.justkac.codechella.naousar.model.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
